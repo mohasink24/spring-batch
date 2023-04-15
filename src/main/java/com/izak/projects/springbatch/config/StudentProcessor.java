82ce55041792664ede521cad8667eb5dff9d454e
mohasink24 <mohasink.24@gmail.com>
2023-04-15 14:28:43 +0530

    WORKING, PROTOTYPE 2

--- src/main/java/com/izak/projects/springbatch/config/StudentProcessor.java
+++ src/main/java/com/izak/projects/springbatch/config/StudentProcessor.java
@@ -5,6 +5,7 @@ import com.izak.projects.springbatch.entity.Student;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.BeanUtils;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
@@ -15,11 +16,10 @@ public class StudentProcessor implements ItemProcessor<StudentBean, Student> {
 
 
     @Override
-    public Student process(final StudentBean studentBean) {
+    public Student process(@Nullable final StudentBean studentBean) {
         if (Objects.isNull(studentBean)) {
             return null;
         }
-
         Student student = new Student();
         BeanUtils.copyProperties(studentBean, student);
         return student;
